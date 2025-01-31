962e15b9d266dd20204ffc40a722ad1f734a4eb3
Petter Reingardt <petter.reingardt@gmail.com>
2025-01-31 11:17:04 +0100

    Jumping is working

--- Assets/Input/PlayerPhysics.cs
+++ Assets/Input/PlayerPhysics.cs
@@ -1,16 +1,33 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerPhysics : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float movementSpeed = 6f;
+    [SerializeField] private float jumpForce = 280;
     private Vector2 velocity = Vector2.zero;
-
+    private Rigidbody rb;
+    private bool isGrounded;
+    private GroundChecker groundChecker;
     public Vector3 LocalSpace { get; set; } = Vector3.zero;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        groundChecker = GetComponentInChildren<GroundChecker>();
+    }
 
     private void Update()
     {
         ApplyMovement();
+        CheckIfGrounded();
+    }
+
+    private void CheckIfGrounded()
+    {
+        isGrounded = groundChecker.IsGrounded;
     }
 
     private void ApplyMovement()
@@ -34,5 +51,12 @@ public class PlayerPhysics : MonoBehaviour
     internal void ChangeVelocity(Vector2 vector2)
     {
         velocity = vector2;
+    }
+
+    internal void Jump(InputValue value)
+    {
+        if (rb == null) Debug.Log("No rigidbody found");
+        if (isGrounded)
+            rb.AddForce(Vector3.up * jumpForce);
     }
 }

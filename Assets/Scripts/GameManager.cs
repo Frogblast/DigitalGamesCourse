e722a0ef7cae5ee7541809b98b2b579d4d85e4c1
Petter Reingardt <petter.reingardt@gmail.com>
2025-01-31 23:58:05 +0100

    added a game initializer for handling the game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Ensure the GameManager persists between scenes
+        }
+        else
+        {
+            Destroy(gameObject); // Ensure only one instance of GameManager exists
+        }
+    }
+
     private void OnEnable()
     {
         EventManager.OnPlayerDeath += GameOver;

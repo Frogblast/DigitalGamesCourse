83399cf2f11c7a2dbf6e1d609c1e5a84c3192d74
Ludde5151 <vakla@hotmail.se>
2025-02-17 11:12:18 +0100

    Added different colors to HP-bar
    Lookin' good

--- Assets/Luddes/Health/Healthbar.cs
+++ Assets/Luddes/Health/Healthbar.cs
@@ -4,16 +4,20 @@ using UnityEngine.UI;
 public class Healthbar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     // Changes the healthbar slider
     public void SetHealth(int health)
     {
         slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+        fill.color = gradient.Evaluate(1f);
     }
 }

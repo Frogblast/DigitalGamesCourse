9ac5b123b7c9cec0e376a716beeee384fd40642d
Ludde5151 <vakla@hotmail.se>
2025-02-24 11:44:59 +0100

    Made logic for a puzzle
    You can now do a small puzzle that will allow the player to reach the cheast afterwards. Scripts and assets now only need a bit of polish.

--- Assets/Luddes/Puzzle/PuzzleTrigger.cs
+++ Assets/Luddes/Puzzle/PuzzleTrigger.cs
@@ -4,7 +4,45 @@ using UnityEngine.Events;
 public class PuzzleTrigger : MonoBehaviour
 {
 
-    PuzzleWall.instance.OnPuzzleUpdated.AddListener();
+    public CustomTrigger keyTrigger;
+    public CustomTrigger GoldBarTrigger;
+
+    private bool isKeyPlaced;
+    private bool isGoldbarPlaced;
+
+    private void Awake()
+    {
+        keyTrigger.EnteredTrigger.AddListener(OnKeyTriggerEntered);
+        GoldBarTrigger.EnteredTrigger.AddListener(OnGoldbarTriggerEntered);
+    }
+
+
+    void OnKeyTriggerEntered(Collider collider)
+    {
+        if (collider.tag == "Key")
+        {
+            Debug.Log("KeyBool is now true");
+            isKeyPlaced = true;
+        }
+    }
+
+    void OnGoldbarTriggerEntered(Collider collider)
+    {
+        if (collider.tag == "GoldBar")
+        {
+            Debug.Log("GoldBar Bool is now true");
+            isGoldbarPlaced = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isKeyPlaced == true && isGoldbarPlaced == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
 
 
 }

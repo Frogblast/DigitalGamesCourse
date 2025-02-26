bd129d40dd19ef99747b83330290175d8b514624
Ludde5151 <vakla@hotmail.se>
2025-02-26 13:52:09 +0100

    Trying to add a hint

--- Assets/Luddes/HiddenPathHint.cs
+++ Assets/Luddes/HiddenPathHint.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class HiddenPathHint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Character")
+        {
+
+        }
     }
 }

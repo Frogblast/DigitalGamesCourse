8c0814f59794e82cb5cce3421089e43d537d5a4a
Rankoin <flix.rolf@gmail.com>
2025-03-03 18:20:16 +0100

    Slightly bigger boulder, Blocked of entrace to escape point, trigger unlock block when finishing luddepussel

--- Assets/Luddes/Puzzle/PuzzleTrigger.cs
+++ Assets/Luddes/Puzzle/PuzzleTrigger.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,11 +8,19 @@ public class PuzzleTrigger : MonoBehaviour
     public CustomTrigger keyTrigger;
     public CustomTrigger GoldBarTrigger;
 
+    public Camera mainCamera;
+    public Camera secondaryCamera;
+    public GameObject gate;
+
+    public float gateRaiseHeight = 5f;
+    public float animationDuration = 2f;
+
     private bool isKeyPlaced;
     private bool isGoldbarPlaced;
 
     private void Awake()
     {
+        secondaryCamera.gameObject.SetActive(false);
         keyTrigger.EnteredTrigger.AddListener(OnKeyTriggerEntered);
         GoldBarTrigger.EnteredTrigger.AddListener(OnGoldbarTriggerEntered);
     }
@@ -28,7 +37,7 @@ public class PuzzleTrigger : MonoBehaviour
     void OnGoldbarTriggerEntered(Collider collider)
     {
         if (collider.tag == "GoldBar")
-        {
+        { 
             isGoldbarPlaced = true;
         }
     }
@@ -37,10 +46,39 @@ public class PuzzleTrigger : MonoBehaviour
     {
         if (isKeyPlaced == true && isGoldbarPlaced == true)
         {
-            gameObject.SetActive(false);
+            StartCoroutine(HandleUnlock());
+            
         }
     }
 
+    // little animation
+    private IEnumerator HandleUnlock()
+    {
+
+        mainCamera.gameObject.SetActive(false); // turn off main camera
+        secondaryCamera.gameObject.SetActive(true); // turn on alternative camera
+
+        Vector3 targetPos = gate.transform.position + new Vector3 (0, gateRaiseHeight, 0); // variables to setup the gate going up function
+        float elapsedTime = 0;
+        Vector3 startPos = gate.transform.position;
+
+        while (elapsedTime < animationDuration)
+        { 
+            gate.transform.position = Vector3.Lerp (startPos, targetPos, elapsedTime/animationDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        gate.transform.position = targetPos; // set it to target after animation is done
+
+        yield return new WaitForSeconds(1f); // chill a little
+
+        secondaryCamera.gameObject.SetActive(false);
+        mainCamera.gameObject.SetActive(true); // switch back
+
+        gameObject.SetActive(false);
+
+    }
 
 
 }

ae041fd9a55f179e08b435c4322f48341fc6591a
Ludde5151 <vakla@hotmail.se>
2025-03-08 12:01:28 +0100

    Added a second hint for invisible path

--- Assets/Luddes/HiddenPathHint.cs
+++ Assets/Luddes/HiddenPathHint.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class HiddenPathHint : MonoBehaviour
 {
 
+    [SerializeField] private int hint1_delay = 20;
+    [SerializeField] private int hint2_delay = 50;
+
     public GameObject hint;
+    public GameObject hint_2;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "Character")
         { 
-            Invoke("showHint", 20f); // shows the hint after 20 seconds of entering the invisible trap
+            Invoke("showHint", hint1_delay); // shows the hint after 20 seconds of entering the invisible trap
+
+            Invoke("showHint_nr2", hint2_delay); // displays the second hint after 40 seconds of entering the invisible trap
         }
     }
 
     private void showHint()
     {
         hint.SetActive(true);
+    }
+
+    private void showHint_nr2()
+    {
+        hint_2.SetActive(true);
     }
     
 }
